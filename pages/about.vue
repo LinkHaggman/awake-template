d8783a016ea6ffc72b665df8115652444df7c79f
Link0w0 <66062268+LinkHaggman@users.noreply.github.com>
2022-08-01 14:49:46 +0200

    update

--- pages/about.vue
+++ pages/about.vue
@@ -2,54 +2,32 @@
   <div id="about-page" class="page-wrapper about-page content-page">
     <site-hero
       title="About Us"
-      subtitle="Example About Page"
+      subtitle="Who we are"
       image="/uploads/about-hero.jpg"
     ></site-hero>
     <main-section theme="sidebar-right">
       <template v-slot:default>
         <div class="content">
-          <h3>Fake Heading</h3>
+          <h2>Our work</h2>
           <p>
             <strong>
-              Edit this page in<code>/pages/about.vue</code>to fit your needs.
+              I'm a furry web developer making websites.
             </strong>
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit!
-          </p>
-
-          <h3>Another Fake Heading</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit!
+            I'm also a Front-End dev and indie game dev from Sweden. I design & code websites, web apps(PWA) & games.
           </p>
           <quote-card />
-          <h3>Still More Fake Content</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit! Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit.
-            <strong>Alias aut facere harum ipsam</strong> quibusdam quidem
-            quisquam saepe? Alias architecto blanditiis consequatur consequuntur
-            dicta ipsa iste modi provident, quod, similique veritatis.
-          </p>
         </div>
 
         <div class="tile is-ancestor">
           <div class="tile is-parent">
             <article class="tile is-child box">
               <p class="title">
-                Cool Stuff We Do
+                Project 1
               </p>
               <p class="subtitle">
-                Sooo Cool
+                Example
               </p>
               <figure class="image is-1by1">
                 <opti-image width="500" height="500" />
@@ -59,10 +37,10 @@
           <div class="tile is-parent">
             <article class="tile is-child box">
               <p class="title">
-                Other Cool Things
+                Project 2
               </p>
               <p class="subtitle">
-                Just as cool
+                Example
               </p>
               <figure class="image is-1by1">
                 <opti-image width="500" height="500" />
@@ -72,10 +50,10 @@
           <div class="tile is-parent">
             <article class="tile is-child box">
               <p class="title">
-                All the cool
+                Project 3
               </p>
               <p class="subtitle">
-                that is all
+                Example
               </p>
               <figure class="image is-1by1">
                 <opti-image width="500" height="500" />
